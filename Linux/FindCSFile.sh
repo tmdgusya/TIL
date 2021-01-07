c1946d379bf2c87b9cb9a2023127ace05edfae10
tmdgusya <tmdgusya@naver.com>
2021-01-07 14:49:58 +0900

    특정 디렉토리 밑의 폴더에 .cs 파일을 찾고 날짜별로 zip파일로 압축하여 backup 폴더에 저장할 수 있도록 한다

---
+++ Linux/FindCSFile.sh
@@ -0,0 +1,51 @@
+#bin/bash
+
+# Requirements
+# Specific Directory : CodeSquad
+# Child Directory : day1 ~ day16
+# Find File : .cs extend File
+# If find .cs extend files, compress to .zip file
+
+# Definition Field
+
+SPECIFIC_DIRECTORY=CodeSquad
+CHILD_DIRECTORY=`ls -all | grep day | awk '{print $9}'`
+TODAY=`date +%Y-%m-%d`
+BACKUP_FOLDER_NAME=backup
+
+# Definition Function
+
+inspectionExistFile() {
+        local CS_FILE=`ls -all | grep .cs | awk '{print $9}'`
+        if [ -z ${CS_FILE} ] ; then
+                echo "${folder} have not .cs file"
+        fi
+}
+
+# Make backup Folder
+mkdir -p ${BACKUP_FOLDER_NAME}/${TODAY}
+
+#Save child Directory to Array
+for folder in ${CHILD_DIRECTORY}
+do
+	cd /${SPECIFIC_DIRECTORY}/${folder}/
+	EXTEND_CS_FILE=`ls -all | grep .cs | awk '{print $9}'`	
+	
+	# If Exist .CS File?
+	inspectionExistFile # func
+
+	for csfile in ${EXTEND_CS_FILE}
+	do
+		if [ -e ${csfile} ] ; then
+			cp -r ${csfile} ../${BACKUP_FOLDER_NAME}/${TODAY}
+		fi
+	done
+done
+
+#Move Directory to Backup Folder
+cd ..
+cd backup/${TODAY}
+
+#Compress File
+zip $TODAY.zip ./*
+
