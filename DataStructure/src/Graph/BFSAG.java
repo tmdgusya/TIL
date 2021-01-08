6a98aa7b4f5587a796329ae2074451099b82c9c6
tmdgusya <tmdgusya@naver.com>
2021-01-08 17:49:19 +0900

    BFS 구현

---
+++ DataStructure/src/Graph/BFSAG.java
@@ -0,0 +1,16 @@
+package Graph;
+
+public class BFSAG {
+
+    private int V;
+    private int[][] matrix;
+    private int[][] visited;
+
+    public BFSAG(int v) {
+        matrix = visited = new int[v][v];
+    }
+
+    public void Edge(int v){
+
+    }
+}
