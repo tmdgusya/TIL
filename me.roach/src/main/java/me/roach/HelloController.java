49252607b3b62220e702462234edbff169d812a3
tmdgusya <tmdgusya@naver.com>
2021-02-21 23:29:20 +0900

    feat :  Link Spring MVC to Servlet

---
+++ me.roach/src/main/java/me/roach/HelloController.java
@@ -0,0 +1,17 @@
+package me.roach;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Autowired
+    HelloService helloService;
+
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Hello, " + helloService.getName();
+    }
+}
---
+++ me.roach/src/main/java/me/roach/WebConfig.java
@@ -0,0 +1,10 @@
+package me.roach;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+@Configuration
+@ComponentScan
+public class WebConfig {
+}
