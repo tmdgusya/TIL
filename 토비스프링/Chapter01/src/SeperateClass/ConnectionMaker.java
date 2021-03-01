0c5c2ec28d2665206e2e699ecff44d49cbbc1414
tmdgusya <tmdgusya@naver.com>
2021-03-01 23:48:51 +0900

    [1회차 토비 스프링 폴더 삭제]

--- 토비스프링/Chapter01/src/SeperateClass/ConnectionMaker.java
+++
@@ -1,9 +0,0 @@
-package SeperateClass;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    public Connection getConnection() throws SQLException, ClassNotFoundException;
-}
