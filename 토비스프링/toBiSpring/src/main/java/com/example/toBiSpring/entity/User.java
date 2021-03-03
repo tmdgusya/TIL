cdd31c0f3f88fbf3a0068fa08c1560d363b748a4
tmdgusya <tmdgusya@naver.com>
2021-03-03 13:40:02 +0900

    feat : Spring Configuration 파일로변경

---
+++ 토비스프링/toBiSpring/src/main/java/com/example/toBiSpring/entity/User.java
@@ -0,0 +1,47 @@
+package com.example.toBiSpring.entity;
+
+import java.util.Objects;
+
+public class User {
+
+    String id;
+    String name;
+    String password;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, password);
+    }
+}
