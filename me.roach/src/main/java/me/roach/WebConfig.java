f21fb8c49ab61bd2e516a3d57c38cf4ff887dde5
tmdgusya <tmdgusya@naver.com>
2021-03-03 21:48:28 +0900

    기본전략 공부

--- me.roach/src/main/java/me/roach/WebConfig.java
+++ me.roach/src/main/java/me/roach/WebConfig.java
@@ -3,14 +3,32 @@ package me.roach;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 public class WebConfig {
+
+    @Bean
+    public HandlerMapping handlerMapping() {
+        RequestMappingHandlerMapping handlerMapping = new RequestMappingHandlerMapping();
+        handlerMapping.setInterceptors(Ordered.HIGHEST_PRECEDENCE);
+        return handlerMapping;
+    }
+
+    @Bean
+    public HandlerAdapter handlerAdapter() {
+        RequestMappingHandlerAdapter handlerAdapter = new RequestMappingHandlerAdapter();
+        return handlerAdapter;
+    }
 
     @Bean
     public ViewResolver viewResolver() {
