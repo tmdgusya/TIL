87695beefc0f4db4e8d2a1bfab525cfa6b0357ea
tmdgusya <tmdgusya@naver.com>
2021-03-03 16:18:11 +0900

    refactor : 전략패턴을 이용하여 중복 코드제거

--- 토비스프링/toBiSpring/src/main/java/com/example/toBiSpring/service/UserDao.java
+++ 토비스프링/toBiSpring/src/main/java/com/example/toBiSpring/service/UserDao.java
@@ -1,54 +1,55 @@
 package com.example.toBiSpring.service;
 
 
-import com.example.toBiSpring.core.ConnectionMaker;
+import com.example.toBiSpring.StatementStartegy.AddStatement;
+import com.example.toBiSpring.StatementStartegy.DeleteAllStatement;
+import com.example.toBiSpring.StatementStartegy.StatementStrategy;
 import com.example.toBiSpring.entity.User;
 
-import java.sql.*;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDao {
 
-    private final ConnectionMaker connectionMaker;
+    private final DataSource dataSource;
 
-    public UserDao(ConnectionMaker connectionMaker) {
-        this.connectionMaker = connectionMaker;
+    public UserDao(DataSource dataSource) {
+        this.dataSource = dataSource;
     }
 
     /**
      * Insert User Entity data at DB User table
+     *
      * @param user (User Entity)
      * @throws ClassNotFoundException
      * @throws SQLException
      */
     public void add(User user) throws ClassNotFoundException, SQLException {
-
-        Connection connection = connectionMaker.makeConnection();
-
-        PreparedStatement ps = connection.prepareStatement("INSERT INTO users(id, name, password) values (?,?,?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-        connection.close();
+        StatementStrategy st = new AddStatement(user);
+        jdbcContextWithStatementStrategy(st);
     }
 
     /**
      * If there's a user tuple in the database with the same ID, Gets the user(tuple) from the database
+     *
      * @param id (user id)
      * @throws ClassNotFoundException
      * @throws SQLException
      */
     public User get(String id) throws ClassNotFoundException, SQLException {
 
-        Connection connection = connectionMaker.makeConnection();
+        Connection connection = dataSource.getConnection();
 
         PreparedStatement ps = connection.prepareStatement("SELECT * FROM users WHERE id = ?");
         ps.setString(1, id);
 
         ResultSet rs = ps.executeQuery();
+
+        if (isNotExistUser(rs)) return null;
+
         rs.next();
 
         User user = new User();
@@ -61,6 +62,30 @@ public class UserDao {
         connection.close();
 
         return user;
+    }
+
+    private boolean isNotExistUser(ResultSet rs) throws SQLException {
+        return !rs.isBeforeFirst();
+    }
+
+    public void deleteAll() throws SQLException {
+        StatementStrategy statementStrategy = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(statementStrategy);
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+            ps.executeUpdate();
+        }catch (SQLException e) {
+            throw e;
+        }finally {
+            c.close();
+            ps.close();
+        }
     }
 
 }
